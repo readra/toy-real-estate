2d6d0f133fbf5322103fbc48108c4acf09b46572
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2023-12-10 20:07:02 +0900

    1. react-bootstrap 적용 시도 (실패, TypeError: Cannot read properties of null (reading 'useContext'))

--- src/main/webapp/reactapp/src/App.js
+++ src/main/webapp/reactapp/src/App.js
@@ -3,41 +3,49 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 function App() {
-  const [messages, setMessages] = useState([]);
+	const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8081/api/apartment")
-        .then((response) => {
-          return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-          setMessages(data);
-        });
-  }, []);
+	useEffect(() => {
+		fetch("http://localhost:8081/api/apartment")
+			.then((response) => {
+				return response.json();
+			})
+			.then(function (data) {
+				console.log(data)
+				setMessages(data);
+			});
+	}, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
 
-        <ul>
-          {messages.map((message) => <p>{message.transactionAmount}</p>)}
-        </ul>
-      </header>
-    </div>
-  );
+				<table>
+					<thead>
+					<tr>
+						<th>#</th>
+						<th>거래금액</th>
+					</tr>
+					</thead>
+					<tbody>
+						{messages.map((message) => <tr><td>1</td><td>{message.transactionAmount}</td></tr>)}
+					</tbody>
+				</table>
+			</header>
+		</div>
+	);
 }
 
 export default App;
