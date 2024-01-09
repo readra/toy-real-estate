bce7c236437e37a8f287a87cb3cf35aaf8f90024
DESKTOP-273P83C\김진용 <yong6302@gmail.com>
2024-01-09 21:14:10 +0900

    1. 시/구/동 Select 추가

--- src/main/webapp/reactapp/src/Main.js
+++ src/main/webapp/reactapp/src/Main.js
@@ -11,24 +11,46 @@ import {
 	TableHead,
 	TableRow
 } from "@mui/material";
+import axios from 'axios';
 
 const Main = () => {
-	const [algorithm, setAlgorithm] = React.useState("");
+	const [si, setSi] = React.useState("");
+	const [gu, setGu] = React.useState("");
+	const [dong, setDong] = React.useState("");
 
-	const handleChange = (event) => {
-		setAlgorithm(event.target.value);
+	const handleSiChange = (event) => {
+		setSi(event.target.value);
+
+	};
+	const handleGuChange = (event) => {
+		setGu(event.target.value);
+	};
+	const handleDongChange = (event) => {
+		setDong(event.target.value);
 	};
 
+
 	const [messages, setMessages] = useState([]);
+	const [sis, setSis] = useState([]);
+	const [gus, setGus] = useState([]);
+	const [dongs, setDongs] = useState([]);
 
 	useEffect(() => {
-		fetch("http://localhost:8081/api/apartment?lawdCode=11740&startYearMonth=2015-01&endYearMonth=2015-02")
-			.then((response) => {
-				return response.json();
-			})
-			.then(function (data) {
-				setMessages(data);
-			});
+		axios
+			.all([axios.get("http://localhost:8081/api/lawd-si"), axios.get("http://localhost:8081/api/lawd-gu"), axios.get("http://localhost:8081/api/lawd-dong")])
+			.then(
+				axios.spread((si, gu, dong) => {
+					setSis(si.data);
+					setGus(gu.data);
+					setDongs(dong.data);
+		}))
+		// fetch("http://localhost:8081/api/apartment?lawdCode=11740&startYearMonth=2015-01&endYearMonth=2015-02")
+		// 	.then((response) => {
+		// 		return response.json();
+		// 	})
+		// 	.then(function (data) {
+		// 		setMessages(data);
+		// 	});
 	}, []);
 
 	return (
@@ -41,19 +63,53 @@ const Main = () => {
 
 			<div style={{ textAlign: "center" }}>
 				<FormControl sx={{ m: 1, minWidth: 200 }}>
-					<InputLabel id="demo-simple-select-label">
-						Algorithm
+					<InputLabel id="si-select-label">
+						지역(시)
 					</InputLabel>
 					<Select
-						labelId="demo-simple-select-label"
-						id="demo-simple-select"
-						value={algorithm}
-						label="Algorithm"
-						onChange={handleChange}
+						labelId="si-select-label"
+						id="si-select"
+						value={si}
+						label="Si"
+						onChange={handleSiChange}
 					>
-						<MenuItem value={1}>Stack</MenuItem>
-						<MenuItem value={2}>Queue</MenuItem>
-						<MenuItem value={3}>Array</MenuItem>
+						{sis.map((si) => (
+							<MenuItem value={1}>{si.name} ({si.code})</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+
+				<FormControl sx={{ m: 1, minWidth: 200 }}>
+					<InputLabel id="gu-select-label">
+						지역(구)
+					</InputLabel>
+					<Select
+						labelId="gu-select-label"
+						id="gu-select"
+						value={gu}
+						label="Gu"
+						onChange={handleGuChange}
+					>
+						{gus.map((gu) => (
+							<MenuItem value={1}>{gu.name} ({gu.code})</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+
+				<FormControl sx={{ m: 1, minWidth: 200 }}>
+					<InputLabel id="dong-select-label">
+						지역(동)
+					</InputLabel>
+					<Select
+						labelId="dong-select-label"
+						id="dong-select"
+						value={dong}
+						label="Dong"
+						onChange={handleDongChange}
+					>
+						{dongs.map((dong) => (
+							<MenuItem value={1}>{dong.name} ({dong.code})</MenuItem>
+						))}
 					</Select>
 				</FormControl>
 			</div>
