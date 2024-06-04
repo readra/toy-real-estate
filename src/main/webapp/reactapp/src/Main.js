40736885aba83ff76d5814ea02b8b0495c3b0b96
readra <yong6302@gmail.com>
2024-06-04 22:05:49 +0900

    1. 다크모드 테마 추가

--- src/main/webapp/reactapp/src/Main.js
+++ src/main/webapp/reactapp/src/Main.js
@@ -15,6 +15,7 @@ import axios from 'axios';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from "moment";
+import DarkModeToggle from "react-dark-mode-toggle";
 
 const Main = () => {
 	const [si, setSi] = React.useState("");
@@ -91,8 +92,15 @@ const Main = () => {
 		setEndDate(moment(selectedDate).format("YYYY년 MM월 DD일"));
 	};
 
+	const [isDarkMode, setIsDarkMode] = useState(() => false);
+
 	return (
 		<>
+			<DarkModeToggle
+				onChange={setIsDarkMode}
+				checked={isDarkMode}
+				size={80}
+			/>
 			<div style={{ textAlign: "center" }}>
 				<FormControl sx={{ m: 1, minWidth: 200 }}>
 					<InputLabel id="si-select-label">
