41e674c034699e1b16366f8de173a15938c4fda3
readra <yong6302@gmail.com>
2024-06-24 22:47:53 +0900

    1. 다크모드 적용중

--- src/main/webapp/reactapp/src/App.js
+++ src/main/webapp/reactapp/src/App.js
@@ -1,9 +1,5 @@
 import React, {Suspense} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Header';
-import Main from './Main';
-import Apartment from './Apartment';
-import NotFound from './NotFound';
 import ThemeProvider from "./context/themeProvider";
 import {GlobalStyle} from "./theme/GlobalStyle";
 import DarkMode from "./components/DarkMode";
@@ -11,25 +7,39 @@ import Sub from "./components/Sub";
 
 const App = () => {
 	return (
-		<div className='App'>
-			<BrowserRouter>
-				<Header />
-				<ThemeProvider>
-					<GlobalStyle />
-					<Suspense fallback={<div>...loading</div>}>
-						<Routes>
-							<Route path="/" component={DarkMode}/>
-							<Route path="/sub" component={Sub}/>
-							<Route path="/main" element={<Main />}/>
-							<Route path="/apartment/*" element={<Apartment />}/>
-							{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-							<Route path="*" element={<NotFound />}/>
-						</Routes>
-					</Suspense>
-				</ThemeProvider>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter>
+			<ThemeProvider>
+				<GlobalStyle />
+				<Suspense fallback={<div>...loading</div>}>
+					<Routes>
+						<Route exact path="/" component={DarkMode}/>
+						<Route exact path="/sub" component={Sub}/>
+					</Routes>
+				</Suspense>
+			</ThemeProvider>
+		</BrowserRouter>
 	);
+
+	// return (
+	// 	<div className='App'>
+	// 		<BrowserRouter>
+	// 			<Header />
+	// 			<ThemeProvider>
+	// 				<GlobalStyle />
+	// 				<Suspense fallback={<div>...loading</div>}>
+	// 					<Routes>
+	// 						<Route path="/" component={DarkMode}/>
+	// 						<Route path="/sub" component={Sub}/>
+	// 						<Route path="/main" element={<Main />}/>
+	// 						<Route path="/apartment/*" element={<Apartment />}/>
+	// 						{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+	// 						<Route path="*" element={<NotFound />}/>
+	// 					</Routes>
+	// 				</Suspense>
+	// 			</ThemeProvider>
+	// 		</BrowserRouter>
+	// 	</div>
+	// );
 };
 
 export default App;
