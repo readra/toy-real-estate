fcc659b061e72c0311a2279aa309b4336320b4c2
readra <yong6302@gmail.com>
2024-06-25 20:31:43 +0900

    1. 다크모드 적용 완료

--- src/main/webapp/reactapp/src/App.js
+++ src/main/webapp/reactapp/src/App.js
@@ -4,6 +4,7 @@ import ThemeProvider from "./context/themeProvider";
 import {GlobalStyle} from "./theme/GlobalStyle";
 import DarkMode from "./components/DarkMode";
 import Sub from "./components/Sub";
+import Main from "./Main";
 
 const App = () => {
 	return (
@@ -12,34 +13,14 @@ const App = () => {
 				<GlobalStyle />
 				<Suspense fallback={<div>...loading</div>}>
 					<Routes>
-						<Route exact path="/" component={DarkMode}/>
-						<Route exact path="/sub" component={Sub}/>
+						<Route path="/" element={<DarkMode />}/>
+						<Route path="/main" element={<Main />}/>
+						<Route path="/sub" element={<Sub />}/>
 					</Routes>
 				</Suspense>
 			</ThemeProvider>
 		</BrowserRouter>
 	);
-
-	// return (
-	// 	<div className='App'>
-	// 		<BrowserRouter>
-	// 			<Header />
-	// 			<ThemeProvider>
-	// 				<GlobalStyle />
-	// 				<Suspense fallback={<div>...loading</div>}>
-	// 					<Routes>
-	// 						<Route path="/" component={DarkMode}/>
-	// 						<Route path="/sub" component={Sub}/>
-	// 						<Route path="/main" element={<Main />}/>
-	// 						<Route path="/apartment/*" element={<Apartment />}/>
-	// 						{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-	// 						<Route path="*" element={<NotFound />}/>
-	// 					</Routes>
-	// 				</Suspense>
-	// 			</ThemeProvider>
-	// 		</BrowserRouter>
-	// 	</div>
-	// );
 };
 
 export default App;
